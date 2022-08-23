706f51a8275cde339af5b718e24f6f4b7472206a
Queasy Banana <111307766+slacktopuss@users.noreply.github.com>
2022-08-23 21:12:48 +0300

    Update y.js

--- y.js
+++ y.js
@@ -1,7 +1,16 @@
+// http прокси берем по ссылке http://hideip.me/ru/proxy/httplist
+// return "PROXY 161.97.130.154:3128"
+
+// socks 4 прокси берем по ссылке http://hideip.me/ru/proxy/socks4list
+// return "SOCKS 82.204.141.94:4145"
+
+// socks 5 прокси берем по ссылке http://hideip.me/ru/proxy/socks5list
+// return "SOCKS5 13.68.221.22:1080"
+
 function FindProxyForURL(url, host) {
 
 var regexpr_torrent = /(^|\.)(meduza\.io|youdo\.com)$/;
-if (regexpr_torrent.test(host)) {return "SOCKS5 194.61.2.102:7497";};
+if (regexpr_torrent.test(host)) {return "PROXY 176.31.68.254:20415";};
 return "DIRECT";
 
 }
