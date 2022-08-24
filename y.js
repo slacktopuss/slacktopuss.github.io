c0961cbd7c876dd2a8f3a42509a3f2a0e7085614
Queasy Banana <111307766+slacktopuss@users.noreply.github.com>
2022-08-24 10:21:36 +0300

    Delete y.js

--- y.js
+++
@@ -1,7 +0,0 @@
-function FindProxyForURL(url, host) {
-
-var regexpr_torrent = /(^|\.)(2ip\.io|2ip\.ru|meduza\.io|youdo\.com)$/;
-if (regexpr_torrent.test(host)) {return "PROXY zerosevenseven.keenetic.link:33311";};
-return "DIRECT";
-
-}
