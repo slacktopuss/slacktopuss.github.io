dccf5687174d47572e8873eb6f117426e910014b
Queasy Banana <111307766+slacktopuss@users.noreply.github.com>
2022-08-29 21:24:55 +0300

    Update y.js

--- y.js
+++ y.js
@@ -1,13 +1,13 @@
 function FindProxyForURL(url, host) {
 
-var regexpr_ua = /(^|\.)(news\.google\.com|paperpaper\.ru|dailymotion\.com|tayga\.info|holod\.media|currenttime\.tv|theins\.ru|the-village\.ru|newtimes\.ru|novayagazeta\.eu|moscowtimes\.ru|republic\.ru|rus\.delfi\.lt|epravda\.com\.ua|thetruestory\.news|eurointegration\.com\.ua|postimees\.ee|unian\.ua|liga\.net|delfi\.lt|err\.ee|svoboda\.org|nv\.ua|tjournal\.ru|bbc\.com|yle\.fi|zona\.media|zerkalo\.io|rutracker\.org|pravda\.com\.ua|meduza\.io)$/;
+var regexpr_ua = /(^|\.)(twitter\.com|news\.google\.com|paperpaper\.ru|dailymotion\.com|tayga\.info|holod\.media|currenttime\.tv|theins\.ru|the-village\.ru|newtimes\.ru|novayagazeta\.eu|moscowtimes\.ru|republic\.ru|rus\.delfi\.lt|epravda\.com\.ua|thetruestory\.news|eurointegration\.com\.ua|postimees\.ee|unian\.ua|liga\.net|delfi\.lt|err\.ee|svoboda\.org|nv\.ua|tjournal\.ru|bbc\.com|yle\.fi|zona\.media|zerkalo\.io|rutracker\.org|pravda\.com\.ua|meduza\.io)$/;
 if (regexpr_ua.test(host))
 {
 
 return "HTTPS proxy-ssl.antizapret.prostovpn.org:3143; PROXY proxy-nossl.antizapret.prostovpn.org:29976; DIRECT";
 };
 
-var regexpr_ru  = /(^|\.)(login.\aliexpress\.com|aliexpress\.ru|amazon\.com|2ip\.ru|youdo\.com)$/;
+var regexpr_ru  = /(^|\.)(forums\.tomsguide\.com|login.\aliexpress\.com|aliexpress\.ru|amazon\.com|2ip\.ru|youdo\.com)$/;
 if(regexpr_ru.test(host))
 {
 
